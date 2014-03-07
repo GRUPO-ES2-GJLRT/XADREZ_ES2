e189d6b784ce3a89d56e267a0966143ff0f9a9a8
Roberto <roberto@ubuntu.(none)>
2014-03-07 16:03:01 -0300

    #2: Botões que aumentam e diminuem o tempo de jogo adicionados na tela não funcionando

--- src/scenes/config_menu.py
+++ src/scenes/config_menu.py
@@ -37,17 +37,47 @@ class ConfigMenu(Scene):
 
         back.click = back_click
 
-        print(os.path.join(self.assets_dir, 'black_king.png'))
-
 
         # Options
-        options = {"minutes_per_game": 1, "moves_per_minutes": 2, "fischer_game": 3}
-        self.option = options["minutes_per_game"]
+        self.options = {"minutes_per_game": 1, "moves_per_minutes": 2, "fischer_game": 3}
+        self.option = self.options["minutes_per_game"]
 
         #Ok Image
         self.ok_position = (50, 200)
         ok = pygame.image.load(os.path.abspath(os.path.join(self.assets_dir, 'ok.png')))
         self.ok = pygame.transform.scale(ok, (50, 50))
+
+        self.minutes = GameText(menu_font, "10", True, (192, 192, 192))
+        self.minutes.rect = self.place_rect(
+            self.minutes.surface,
+            self.game.relative_x(0.85),
+            self.game.relative_y(0.30),
+        )
+
+        # Plus Button
+        plus = GameText(menu_font, PLUS, True, (128, 128, 128))
+        plus.rect = self.place_rect(
+            plus.surface,
+            self.game.relative_x(0.80),
+            self.game.relative_y(0.30),
+        )
+
+        # Minus Button
+        minus = GameText(menu_font, MINUS, True, (128, 128, 128))
+        minus.rect = self.place_rect(
+            minus.surface,
+            self.game.relative_x(0.90),
+            self.game.relative_y(0.30),
+        )
+
+        def plus_click(game):
+            self.minutes.text = str(int(self.minutes.text) + 1)
+
+        def minus_click(game):
+            self.minutes.text = str(int(self.minutes.text) - 1)
+
+        plus.click = plus_click
+        minus.click = minus_click
 
         # Minutes_per_Game Button
         minutes_per_game = GameText(menu_font, MINUTES_PER_GAME, True, (128, 128, 128))
@@ -59,6 +89,7 @@ class ConfigMenu(Scene):
 
         def minutes_per_game_click(game):
             self.ok_position = (50, 200)
+            self.option = self.options["minutes_per_game"]
 
         minutes_per_game.click = minutes_per_game_click;
 
@@ -72,6 +103,7 @@ class ConfigMenu(Scene):
 
         def moves_per_minutes_click(game):
             self.ok_position = (50, 280)
+            self.option = self.options["moves_per_minutes"]
 
         moves_per_minutes.click = moves_per_minutes_click;
 
@@ -85,6 +117,7 @@ class ConfigMenu(Scene):
 
         def fischer_time_click(game):
             self.ok_position = (50, 360)
+            self.option = self.options["fischer_game"]
 
         fischer_time.click = fischer_time_click;
 
@@ -101,8 +134,8 @@ class ConfigMenu(Scene):
 
         quit.click = quit_click
 
-        self.texts = [title, back, quit, minutes_per_game, moves_per_minutes, fischer_time]
-        self.buttons = [back, quit, minutes_per_game, moves_per_minutes, fischer_time]
+        self.texts = [title, back, quit, minutes_per_game, moves_per_minutes, fischer_time, self.minutes, plus, minus]
+        self.buttons = [back, quit, minutes_per_game, moves_per_minutes, fischer_time, plus, minus]
     
     def draw(self, delta_time):
         """Draws MainMenu"""
