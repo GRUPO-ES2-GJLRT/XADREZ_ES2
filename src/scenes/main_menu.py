ec7b4dcb8487f23ca0e7fe77a19f6c46da47b83a
Joao Felipe <joaofelipenp@gmail.com>
2014-03-23 12:12:36 -0300

    #6 corrigindo botao de dois jogadores

--- src/scenes/main_menu.py
+++ src/scenes/main_menu.py
@@ -32,7 +32,7 @@ class MainMenu(Scene):
         )
 
         def one_player_click(game):
-            game.scene = Chess(game) #TODO: Change to One Player Scene
+            game.scene = Scene(game) #TODO: Change to One Player Scene
 
         one_player.click = one_player_click
 
@@ -44,7 +44,7 @@ class MainMenu(Scene):
         )
 
         def two_players_click(game):
-            game.scene = Scene(game) #TODO: Change to Two Players Scene
+            game.scene = Chess(game) #TODO: Change to Two Players Scene
 
         two_players.click = two_players_click
 
